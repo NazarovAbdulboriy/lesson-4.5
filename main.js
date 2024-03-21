ae050bf39d98b5a16dd393910fb3103c7ed58284
NazarovAbdulboriy <nazarovabdulboriy377@gmail.com>
2024-03-21 09:26:55 +0500

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let name1 = prompt("Ismingizni kiriting?");
-let letter = prompt("Ihtiyoriy harf kiriting?");
+let name1 = prompt("Ismingizni kiriting!")
+let letter = prompt("Istagan harfni kiriting!")
 
-let check = name1.includes(letter);
+let check = name1.toLowerCase().includes(letter.toLowerCase());
 
 if (check) {
-    alert("Siz kiritgan harf mavjud")
-}else(
-    alert("Siz kiritgan harf mavjud emas....")
+    alert("Siz kirtgan harf mavjud")
+} else (
+    alert("Siz kiritgan harf mavjud emas..")
 )
